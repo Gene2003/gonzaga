e3e4d5b8fcc945ef650e01056c218d670ac93d6e
Gonzaga Shyachi <gonzagagene80@gmail.com>
2025-12-06 22:22:06 +0300

    yes

--- 024globalconnect/src/api/services/authService.js
+++ 024globalconnect/src/api/services/authService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import apiClient from '../client';
 import { API_ENDPOINTS } from '../endpoints';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const authService = {
   login: async ({ username, password }) => {
