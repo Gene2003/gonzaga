ce5d54352fe9e7f252cf1e7f0709edb134423a42
Gene2003 <gonzagagene80@gmail.com>
2026-02-02 16:46:55 +0300

    yes

--- backend/globalconnect024/globalconnect024/settings.py
+++ backend/globalconnect024/globalconnect024/settings.py
@@ -109,6 +109,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    "https://www.024global.com",
+    "https://024globalconnect-backend-production.up.railway.app",
     "http://localhost:8000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
