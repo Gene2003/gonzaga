dfb241d3a93b28a8bcc1afec158ec790dbf42cd9
Gene2003 <gonzagagene80@gmail.com>
2026-02-14 16:25:26 +0300

    yes

--- 024globalconnect/src/components/login/About.jsx
+++ 024globalconnect/src/components/login/About.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { use } from "react";
 
 const About = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  //images
+  const slides = [
+    '/images/1.png',
+    '/images/2.png',
+    '/images/3.png',
+    '/images/4.png',
+    '/images/5.png',
+  ];
+  // Auto-slide every 3 seconds
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [slides.length]);
+
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
+
+  const goToNextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % slides.length);
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+  
   return (
     <section className="w-full py-20 flex justify-center bg-white">
       <div className="max-w-4xl px-6 text-center md:text-left">
