eb2256be2a1b6691ae93bd9576f6ba40eadf8d26
Gene2003 <gonzagagene80@gmail.com>
2026-03-07 15:58:17 +0300

    add missing referral columns

---
+++ backend/globalconnect024/orders/migrations/0009_fix_referral_columns.py
@@ -0,0 +1,32 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0008_create_missing_tables'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='referral',
+            name='commission_rate',
+            field=models.DecimalField(decimal_places=2, default=5.0, max_digits=5),
+        ),
+        migrations.AddField(
+            model_name='referral',
+            name='approved_at',
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='referral',
+            name='paid_at',
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='referral',
+            name='payment_split',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='referral', to='orders.paymentsplit'),
+        ),
+    ]
